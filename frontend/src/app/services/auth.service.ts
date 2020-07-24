c6c9a3b26091eadf66afb6a8931d4f2dba6eeb21
tlesetla <thato@thedigitalacademy.co.za>
2020-07-25 01:49:23 +0200

    Add login service

---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  isLoggedIn;
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string) {
+    this.http.post(`${environment.url}/login`, {email: email, pass: password}).pipe(
+      map(user => {
+        // login successful if there's a jwt token in response
+        if (user && user.access_token) {
+          // store user details and jwt token in local storage to keep user logged in
+          localStorage.setItem('currentUser', JSON.stringify(user.access_token));
+          this.isLoggedIn = true;         // set user login status
+        }
+        return user;
+      })
+    );
+  }
+}
